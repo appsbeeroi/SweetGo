cad3c5708d1f6baf6083a66d7505a1b54791e961
esplusnin <esplusnin94@yandex.ru>
2025-09-22 20:29:14 +0300

    Initial Commit

---
+++ SweetGo/SweetGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetGoApp.swift
+//  SweetGo
+//
+//  Created by Евгений on 22.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct SweetGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
